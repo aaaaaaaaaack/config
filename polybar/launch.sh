5e980fdeaaeae9449ea19acd5f477fb99af08452
Eliser Gio Alonzo <eliser.gio.alonzo@gmail.com>
2022-07-23 15:41:32 +0800

    Curated config file

---
+++ polybar/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+polybar left -c ~/.config/polybar/left &
+polybar center -c ~/.config/polybar/center &
+polybar right -c ~/.config/polybar/right &
+# polybar tray -c ~/.config/polybar/tray &
